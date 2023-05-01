b975f7555f9582426845d93dc43f8bbb71ac8bf1
Dimass16 <dmitkach1@gmail.com>
2023-05-02 00:46:55 +0300

    commit 2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,11 +1,4 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+
 
 
 import Notiflix from 'notiflix';
@@ -49,3 +42,13 @@ function createPromise(position, delay) {
 
   return promise;
 }
+
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
